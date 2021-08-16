cd1ffa5ca3281919f41cf636e7c5f51e2459e6d3
beauxvice <gregybos@gmail.com>
2021-08-16 17:14:40 -0500

    making  db index.ts and config files.

--- server/db/index.ts
+++ server/db/index.ts
@@ -1,1 +1,12 @@
-// init
+import { Pool, Client } from 'pg';
+import { PORT } from './config';
+
+const port = PORT || 3000;
+
+const credentials = {
+  user: "postgres",
+  host: "localhost",
+  database: "nodedemo",
+  password: "yourpassword",
+  port: 5432,
+};
