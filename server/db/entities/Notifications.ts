d81cefeaf4fe89d1c55e36f2d6292b2eee8605df
calebbbbs <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>
2021-09-22 11:07:56 -0500

    pulled from upstream, laying groundwork for notification tray

--- server/db/entities/Notifications.ts
+++ server/db/entities/Notifications.ts
@@ -1,41 +1,41 @@
-import { Entity, PrimaryGeneratedColumn, Column, BaseEntity, ManyToOne } from 'typeorm';
-import { ObjectType, Field, ID } from 'type-graphql';
-import Friend from './Friend';
-import User from './User';
+// import { Entity, PrimaryGeneratedColumn, Column, BaseEntity, ManyToOne } from 'typeorm';
+// import { ObjectType, Field, ID } from 'type-graphql';
+// import Friend from './Friend';
+// import User from './User';
 
-@Entity()
-@ObjectType()
-export default class Notification extends BaseEntity {
-  @Field(() => String)
-  @PrimaryGeneratedColumn()
-  id: string;
+// @Entity()
+// @ObjectType()
+// export default class Notification extends BaseEntity {
+//   @Field(() => String)
+//   @PrimaryGeneratedColumn()
+//   id: string;
 
-  @Column()
-  @Field(() => ID)
-  user_id: String;
+//   @Column()
+//   @Field(() => ID)
+//   user_id: String;
 
-  @Field(() => String)
-  @Column()
-  friend_id: string;
+//   @Field(() => String)
+//   @Column()
+//   friend_id: string;
 
-  @Field(() => String)
-  @Column()
-  friend_name: string;
+//   @Field(() => String)
+//   @Column()
+//   friend_name: string;
 
-  @Field(() => String)
-  @Column()
-  status: string;
+//   @Field(() => String)
+//   @Column()
+//   status: string;
 
-  @Field(() => String)
-  @Column()
-  action: string;
+//   @Field(() => String)
+//   @Column()
+//   action: string;
 
-  @Field(() => String)
-  @Column()
-  message: string;
+//   @Field(() => String)
+//   @Column()
+//   message: string;
 
-  @Field(() => User)
-  @ManyToOne(() => User, (user: User) => user.notif,
-  {cascade: true})
-  user!: Promise<User | undefined>;
-}
+//   @Field(() => User)
+//   @ManyToOne(() => User, (user: User) => user.notif,
+//   {cascade: true})
+//   user!: Promise<User | undefined>;
+// }
