d9a1280cf873dcd6bdd9c8c9c31d521b13fb8a02
calebbbbs <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>
2021-09-26 23:17:59 -0500

    menu stays open until closed with menu button or x

--- client/src/components/Main/NotifMenu.tsx
+++ client/src/components/Main/NotifMenu.tsx
@@ -10,13 +10,18 @@ import {
   IconButton,
   useColorModeValue,
   Flex,
+  Link,
+  useDisclosure,
+  // useBreakpointValue
   // Button
 } from '@chakra-ui/react';
 
 import { UserContext } from '../../contexts/UserContext';
 import { gql, useMutation } from "@apollo/client";
-
+import { CloseIcon } from "@chakra-ui/icons";
 import { BiBell } from 'react-icons/bi';
+
+
 
 const REMOVE_NOTIFICATION = gql`
  mutation RemoveNotificationMutation($removeNotificationData: RemoveNotificationInput!) {
@@ -27,9 +32,11 @@ const REMOVE_NOTIFICATION = gql`
 const NotifMenu = (props: any) => {
   const { isLoggedIn, userObj, refetch }: any = useContext(UserContext);
   const [removeNotification] = useMutation(REMOVE_NOTIFICATION);
-
+  const { onOpen, onClose } = useDisclosure();
+  // const opts = { base: 2, sm: 3, md: 3, lg: 3, xl: 6 }
+  // const tracksPerPage = useBreakpointValue(opts) || 2;
   const list = userObj.notifications.map((e: any, i: number) => {
-    return <MenuItem key={i} onClick={() =>{
+    return <MenuItem key={i} closeOnSelect={false} onClick={() =>{
       removeNotification({
         variables: {
           removeNotificationData: {
@@ -43,18 +50,22 @@ const NotifMenu = (props: any) => {
   }).reverse();
     return (
       <Menu>
-
         <MenuButton
           m={4}
           as={IconButton}
           aria-label='Options'
           icon={<BiBell />}
           variant='ghost'
+          onClick={onOpen}
         />
         <MenuList bg={useColorModeValue('brand.50', 'brand.900')} zIndex={2}>
           <MenuGroup>
-            <Flex>
+            <Flex justifyContent="space-between" >
             <chakra.div m={2}>Notifications</chakra.div>
+            <Flex>
+              <MenuItem onClick={onClose}><CloseIcon />
+              </MenuItem>
+              </Flex>
             </Flex>
             <MenuDivider />
             {isLoggedIn && (
@@ -62,6 +73,8 @@ const NotifMenu = (props: any) => {
                 {list}
               </chakra.div>
             )}
+
+            {isLoggedIn && userObj.notifications.length > 0 && (<Link m={2}>See More</Link>)}
           </MenuGroup>
           {/* <MenuDivider /> */}
           <MenuGroup></MenuGroup>
